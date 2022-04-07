7cb8a0bfcff2a42fe70f6eabf543af30dc444333
abelazumgui <abel_13_mm@hotmail.com>
2022-04-07 09:48:36 +0200

    first commit

---
+++ src/test/java/com/example/demo/HolaGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
